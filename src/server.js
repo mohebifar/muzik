74168cfc86e0ececa14070a07f109a062ae49f9b
mohebifar <netfars@gmail.com>
2016-03-25 11:12:43 +0430

    chore: Production configuration and heroku setup

--- src/server.js
+++ src/server.js
@@ -6,16 +6,24 @@ import { port } from 'config';
 
 const app = express();
 
-app.use((req, res) => {
-  const assets = {
-    styles: [
-      'https://fonts.googleapis.com/css?family=Roboto:400,300,500'
-    ],
-    scripts: [
-      'http://localhost:8080/main.js'
-    ]
-  };
+const assets = {
+  styles: [
+    'https://fonts.googleapis.com/css?family=Roboto:400,300,500'
+  ],
+  scripts: [
+    process.env.NODE_ENV === 'development' ?
+      'http://localhost:8080/main.js' :
+      '/dist/main.js'
+  ]
+};
 
+if (process.env.NODE_ENV === 'production') {
+  assets.styles.push('/dist/main.css');
+}
+
+app.use(express.static(__dirname + '/../static'));
+
+app.use((req, res) => {
   const element = <Html assets={assets}/>;
   res.send(ReactDOM.renderToString(element));
 });
